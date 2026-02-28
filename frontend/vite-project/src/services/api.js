b9ecc7374b1985ddbcb8caa06b0daf82178cb983
tulabhagya2 <tulabhagyasri1@gmail.com>
2026-02-28 23:31:56 +0530

    frontend commit

--- frontend/vite-project/src/services/api.js
+++ frontend/vite-project/src/services/api.js
@@ -1,5 +1,5 @@
 const API_BASE_URL =
-  import.meta.env.VITE_API_BASE_URL || "https://healthy-habits-tracker-backend-repo.onrender.com/";
+  import.meta.env.VITE_API_BASE_URL || "https://healthy-habits-tracker-backend-repo.onrender.com";
 
 /* =========================================
    TOKEN + HEADERS
